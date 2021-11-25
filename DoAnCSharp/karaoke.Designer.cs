012b91d2803e8e134fee39ff2af9c6b65e360e52
Đạt Nguyễn Thành <18520580@ms.uit.edu.vn>
2021-11-25 21:28:29 +0700

    tam on lyric karaoke

--- DoAnCSharp/karaoke.Designer.cs
+++ DoAnCSharp/karaoke.Designer.cs
@@ -32,27 +32,22 @@ namespace DoAnCSharp
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(karaoke));
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.hopeTrackBar1 = new ReaLTaiizor.Controls.HopeTrackBar();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(678, 54);
+            this.button1.Location = new System.Drawing.Point(678, 69);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 40);
             this.button1.TabIndex = 1;
@@ -60,39 +55,10 @@ namespace DoAnCSharp
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(678, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(118, 22);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "ZU7C8FDU";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(713, 229);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 32);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(716, 312);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 32);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
-            // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(878, 152);
+            this.button2.Location = new System.Drawing.Point(678, 143);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(118, 40);
             this.button2.TabIndex = 5;
@@ -125,10 +91,6 @@ namespace DoAnCSharp
             this.hopeTrackBar1.UnknownColor = System.Drawing.Color.White;
             this.hopeTrackBar1.Value = 0;
             // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -157,6 +119,7 @@ namespace DoAnCSharp
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
             this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(251, 102);
             this.axWindowsMediaPlayer1.TabIndex = 9;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
             // 
             // panel1
             // 
@@ -166,55 +129,46 @@ namespace DoAnCSharp
             this.panel1.Size = new System.Drawing.Size(558, 499);
             this.panel1.TabIndex = 10;
             // 
-            // button3
+            // timer1
             // 
-            this.button3.Location = new System.Drawing.Point(607, 130);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 29);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Clear";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.timer1.Interval = 110;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // button4
+            // panel2
             // 
-            this.button4.Location = new System.Drawing.Point(607, 184);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 29);
-            this.button4.TabIndex = 12;
-            this.button4.Text = "Draw";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.panel2.Location = new System.Drawing.Point(706, 277);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(188, 46);
+            this.panel2.TabIndex = 11;
             // 
-            // numericUpDown1
+            // comboBox1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(879, 67);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
-            this.numericUpDown1.TabIndex = 13;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Phi hành gia - ZU9CE99E",
+            "Thay lòng - ZU7C8FDU"});
+            this.comboBox1.Location = new System.Drawing.Point(678, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Text = "Phi hành gia - ZU9CE99E";
             // 
             // karaoke
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1128, 603);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.hopeTrackBar1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "karaoke";
             this.Text = "karaoke";
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -222,18 +176,14 @@ namespace DoAnCSharp
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
         private ReaLTaiizor.Controls.HopeTrackBar hopeTrackBar1;
-        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- DoAnCSharp/karaoke.cs
+++ DoAnCSharp/karaoke.cs
@@ -11,6 +11,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace DoAnCSharp
@@ -22,61 +23,89 @@ namespace DoAnCSharp
         int idx1 = 0;
         int idx2 = 0;
 
+        string link;
+
+        Graphics g;
+
         public karaoke()
         {
             InitializeComponent();
+            g = panel1.CreateGraphics();
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
+            string songID = comboBox1.SelectedItem.ToString().Split('-')[1].Trim();
             HttpClient httpClient = new HttpClient();
-            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, $"https://dat-zing-mp3-api.herokuapp.com/song/lyric/{textBox1.Text}"); ;
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, $"https://dat-zing-mp3-api.herokuapp.com/song/lyric/{songID}");
             HttpResponseMessage response = await httpClient.SendAsync(requestMessage);
             lyric = JsonConvert.DeserializeObject<Lyric>(await response.Content.ReadAsStringAsync());
             label3.Text = "0";
             var words = lyric.sentences[lyric.sentences.Count - 1].words;
             label4.Text = words[words.Count - 1].endTime.ToString();
             hopeTrackBar1.MaxValue = words[words.Count - 1].endTime;
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, $"https://dat-zing-mp3-api.herokuapp.com/song/stream/{songID}");
+            response = await httpClient.SendAsync(req);
+            link = JsonConvert.DeserializeObject<Streaming>(await response.Content.ReadAsStringAsync())._128;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string txt = lyric.sentences[idx1].fullSentence() + "\n" + lyric.sentences[idx1 + 1].fullSentence();
-            Draw(panel1, panel1.CreateGraphics(), txt, -1, Color.Blue, Color.Black);
-            axWindowsMediaPlayer1.URL = "C:\\Users\\DAT\\Desktop\\music\\thaylong.mp3";
-            timer1.Start();
+            //string txt = lyric.sentences[idx1].fullSentence() + "\n" + lyric.sentences[idx1 + 1].fullSentence();
+            //Draw(panel1, panel1.CreateGraphics(), txt, -1, Color.Blue, Color.Black);
+            axWindowsMediaPlayer1.URL = link;
+            startTime = lyric.sentences[0].words[0].startTime;
+            endTime = lyric.sentences[1].words.Last().endTime;
+
         }
 
-        private async void timer1_Tick(object sender, EventArgs e)
+        private int startTime;
+        private int endTime;
+
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            t += timer1.Interval;
-            hopeTrackBar1.Value = t;
-            label3.Text = t.ToString();
-            if (t >= int.Parse(label4.Text) || idx1 >= lyric.sentences.Count)
-            {
-                timer1.Stop();
-                return;
-            }
 
-            if (t >= lyric.sentences[idx1].words[idx2].startTime && t < lyric.sentences[idx1].words[idx2].endTime)
+            if (t >= startTime && t < endTime)
             {
-                panel1.Refresh();
-                string txt = lyric.sentences[idx1].fullSentence() + "\n" + lyric.sentences[idx1 + 1].fullSentence();
-                await Draw(panel1, panel1.CreateGraphics(), txt, idx2, Color.Blue, Color.Black);
+
+                int temp = idx2;
+                int temp2 = idx1;
+                if (temp >= lyric.sentences[idx1].words.Count)
+                {
+                    temp = idx2 - lyric.sentences[idx1].words.Count;
+                    temp2 += 1;
+                }
+                if (t >= lyric.sentences[temp2].words[temp].endTime)
+                {
+                    idx2++;
+                    string text = lyric.sentences[idx1].fullSentence() + " \n" + lyric.sentences[idx1 + 1].fullSentence();
+                    panel1.Refresh();
+                    Draw(panel1, g, text, idx2, Color.Blue, Color.Black);
+                }
+
             }
-            else if (t >= lyric.sentences[idx1].words[idx2].endTime)
+            if (t >= endTime - timer1.Interval)
             {
-                idx2++;
-            }
-            if (idx2 >= lyric.sentences[idx1].words.Count)
-            {
-                idx1++;
+                idx1 += 2;
                 idx2 = 0;
+                if (idx1 >= lyric.sentences.Count)
+                {
+                    timer1.Stop();
+                    return;
+                }
+                startTime = lyric.sentences[idx1].words[0].startTime;
+                endTime = lyric.sentences[idx1 + 1].words.Last().endTime;
             }
 
+            t += timer1.Interval + 15;
         }
 
-        private async Task<int> Draw(Control control, Graphics g, string text, int idx, Color color1, Color color2)
+
+
+
+
+        private void Draw(Control control, Graphics g, string text, int idx, Color color1, Color color2)
         {
             TextFormatFlags flags = TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter |
                                     TextFormatFlags.NoPadding | TextFormatFlags.NoClipping;
@@ -98,31 +127,22 @@ namespace DoAnCSharp
                     {
                         Rectangle WordBounds = Rectangle.Round(regions[i].GetBounds(g));
                         string word = text.Substring(ranges[i].First, ranges[i].Length);
-                        Color c = i == idx ? color1 : color2;
+                        Color c = i <= idx ? color1 : color2;
                         TextRenderer.DrawText(g, word, font, WordBounds, c, flags);
-
                     }
                 }
             }
-            return 1;
+
         }
-
-
-        private void button4_Click(object sender, EventArgs e)
+        bool start = true;
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
         {
-            string txt = "Dat Dep trai bo doi\nCan ca the gioi";
-            Draw(panel1, panel1.CreateGraphics(), txt, 2, Color.Blue, Color.Black);
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            panel1.Invalidate();
-        }
-
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
-            string txt = "Dat Dep trai bo doi\nCan ca the gioi";
-            Draw(panel1, panel1.CreateGraphics(), txt, Convert.ToInt32(numericUpDown1.Value), Color.Blue, Color.Black);
+            if (start && axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                t = 0;
+                timer1.Start();
+                start = false;
+            }
         }
     }
 }
